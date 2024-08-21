b48d5250ca8f0c302ce87efaad7ef8d09198f7ac
aryaidnani <idnaniarya@gmail.com>
2024-08-21 12:16:19 +0530

    Improved accordion icons

--- about-script.js
+++ about-script.js
@@ -1,10 +1,14 @@
 const itemSelector = document.querySelectorAll(".item");
 const daySelector = document.querySelectorAll(".day-number");
 const dateSelector = document.querySelectorAll(".date-number");
+const downIcon = document.querySelectorAll(".downwards");
+const upIcon = document.querySelectorAll(".upwards");
 
 for (let i = 0; i < itemSelector.length; i++) {
   itemSelector[i].addEventListener("click", function () {
     itemSelector[i].classList.toggle("active");
+    downIcon[i].classList.toggle("hidden");
+    upIcon[i].classList.toggle("hidden");
   });
 
   itemSelector[i].addEventListener("mouseenter", function () {
